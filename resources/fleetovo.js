3ca0348ee25ee5e03c4c0e3a0342be01c884b847
amit-kmishra <amit884084@gmail.com>
2024-11-17 18:05:52 +0530

    Initial commit

---
+++ resources/fleetovo.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#myTable").each(function (_, table) {
+        $(table).DataTable();
+    });
+});
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
